35a098bc463cdabdb7c3c4b2fd7002b3472006bf
Ghostik-gh <feodor200@mail.ru>
2022-09-02 23:50:11 +0300

    not found & styles

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,14 @@
 import React, { FC } from 'react';
+import img from '../../assets/images/404.jpg';
+import styles from './NotFound.module.scss';
 
 export const NotFound: FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <img src={img} alt="Error 404" />
+        <h1>Page not Found</h1>
+      </div>
+    </>
+  );
 };
