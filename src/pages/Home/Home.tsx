2f2ee55d265e90511a6468971ed0cbeea7fe3d70
Ghostik-gh <feodor200@mail.ru>
2022-09-15 20:13:04 +0300

    home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { Symptoms } from '../../components/Symptoms/Symptoms';
+
+export const Home: FC = () => {
+  return (
+    <>
+      <Symptoms />
+    </>
+  );
+};
