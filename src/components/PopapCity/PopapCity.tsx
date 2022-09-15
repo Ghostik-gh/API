7db7811bda1fbb688a9396ec2ebc8bba9ed65bd3
Ghostik-gh <feodor200@mail.ru>
2022-09-15 20:12:33 +0300

    close btn

--- src/components/PopapCity/PopapCity.tsx
+++ src/components/PopapCity/PopapCity.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from 'react';
+import { useAppDispatch } from '../../hooks/redux';
+import { userSlice } from '../../store/reducers/user/userSlice';
 import styles from './PopapCity.module.scss';
 
 export const PopapCity: FC = () => {
+  const dispatch = useAppDispatch();
+
+  const closePopap = () => {
+    dispatch(userSlice.actions.clickExit());
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.window}>
-        <div className={styles.content}>
-          <div className={styles.title}>Выберите свой город:</div>
-          <input placeholder="city" />
+    <>
+      <div className={styles.close} onClick={closePopap}>
+        X
+      </div>
+      <div className={styles.container}>
+        <div className={styles.window}>
+          <div className={styles.content}>
+            <div className={styles.title}>Выберите свой город:</div>
+            <input placeholder="city" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
